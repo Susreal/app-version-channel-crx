1b5c565a34dc49da7e6e033f26ed88528a948892
Susreal Zhao <zhaoxreg@163.com>
2019-07-31 22:34:31 +0800

    😂 layui的数据读写有点坑
    😂 layui的数据读写有点坑

--- js/index.js
+++ js/index.js
@@ -6,6 +6,47 @@ var appInfo = {
     sougouId: "667320"
 }
 
+var default_app_info = {
+    app_name: "yaktalk",
+    shop_info: [
+        {
+            id: "SHOP_IOS",
+            shop_name: "App Store",
+            url: "https://apps.apple.com/cn/app/id1404845879"
+        },
+        {
+            id: "SHOP_YYB",
+            shop_name: "应用宝",
+            url: "https://sj.qq.com/myapp/detail.htm?apkName=com.cccampus.younglife"
+        },
+        {
+            id: "SHOP_MI",
+            shop_name: "小米",
+            url: "http://app.mi.com/details?id=com.cccampus.younglife"
+        },
+        {
+            id: "SHOP_HW",
+            shop_name: "华为",
+            url: "https://appstore.huawei.com/app/C100295325"
+        },
+        {
+            id: "SHOP_MZ",
+            shop_name: "魅族",
+            url: "http://app.meizu.com/apps/public/detail?package_name=com.cccampus.younglife"
+        },
+        {
+            id: "SHOP_BD",
+            shop_name: "百度",
+            url: "https://shouji.baidu.com/software/26255558.html"
+        },
+        {
+            id: "SHOP_SG",
+            shop_name: "搜狗",
+            url: "http://zhushou.sogou.com/apps/detail/667320.html"
+        }
+    ]
+}
+
 var allShops = [
     "IOS",
     "YINGYONGBAO",
@@ -130,7 +171,23 @@ $("#save").click(function() {
     chrome.storage.local.set({'app_version_in_shops_info': tInfo}, function() {
         console.log('保存成功!!!');
     });
+    chrome.storage.local.set({'app_version_in_shops_info_all': default_app_info}, function() {
+        chrome.storage.local.get(['app_version_in_shops_info_all'], function(result) {
+            let ttt = result.app_version_in_shops_info_all;
+            console.log(ttt.app_name);
+            console.log(ttt.shop_info.length);
+        });
+    });
 }); 
+
+function updateForm(layer_filter, name, value) {
+    layui.use(['form'], function(){
+        var form = layui.form;
+        form.val(layer_filter, {
+            name: value
+        });
+    });
+}
 
 $("#search").click(function() {
     chrome.storage.local.get(['app_version_in_shops_info'], function(aInfo) {
@@ -138,6 +195,16 @@ $("#search").click(function() {
             updateLatestVerion(aInfo.app_version_in_shops_info, allShops[i]);
         }
     });
+    chrome.storage.local.get(['app_version_in_shops_info_all'], function(result) {
+        let ttt = result.app_version_in_shops_info_all;
+        for(let i=0; i<ttt.shop_info.length; i++) {
+            console.log(ttt.shop_info[i].id);
+            console.log(ttt.shop_info[i].url);
+            updateForm("shops",ttt.shop_info[i].id,ttt.shop_info[i].url);
+        }
+
+    });
+    updateForm("shops","SHOP_IOS","LALALAL");
 });
 
 
--- js/initLayui.js
+++ js/initLayui.js
@@ -4,16 +4,53 @@ layui.use(['layer', 'form', 'element', 'table'], function(){
     ,element = layui.element
     ,table = layui.table;
     
-    // ayer.msg('Hello World');
+    // layer.msg('Hello World');
 
-    form.val('example', {
-        "username": "贤心"
+    form.val('search', {
+        "title": "贤心"
     });
+
+    // //默认prompt
+    // layer.prompt(function(val, index){
+    //     layer.msg('得到了'+val);
+    //     layer.close(index);
+    // });
+
+    // //第一个实例
+    // table.render({
+    //     elem: '#shops'
+    //     ,url: 'defaultTable.json' //数据接口
+    //     ,cols: [[ //表头
+    //     {field: 'id', title: 'ID', width:50}
+    //     ,{field: 'shop', title: '渠道', width:120}
+    //     ,{field: 'url', title: '爬虫链接（点击编辑）', edit: 'text'}
+    //     ]]
+    // });
+
+    // //监听单元格编辑
+    // table.on('edit(demoEvent)', function(obj){
+    //     var value = obj.value //得到修改后的值
+    //     ,data = obj.data //得到所在行所有键值
+    //     ,field = obj.field; //得到字段
+    //     layer.msg('[ID: '+ data.id +'] ' + field + ' 字段更改为：'+ value, {offset: 't'});
+
+    // });
+
 });
 
 
-$('#appName').bind('input propertychange', function() {  
-    let txt = $('#appName').val();
-    if(txt) $('#labelName').text("valid");
-    else $('#labelName').text("invalid");
+$('#app-name').bind('input propertychange', function() {  
+    let txt = $('#app-name').val();
+    if(txt) {
+        if ($('#search-btn').hasClass("layui-btn-disabled")) {
+            $('#search-btn').removeClass("layui-btn-disabled");
+        }
+        $('#label-name').text("valid");
+    }
+    else {
+        if (!$('#search-btn').hasClass("layui-btn-disabled")) {
+            $('#search-btn').addClass("layui-btn-disabled");
+        }
+        $('#label-name').text("invalid");
+    }
 });
